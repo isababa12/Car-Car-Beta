e36eceb3d8976b8589e4f7526b7f744dad953bb3
Ryan <ryanlu294@gmail.com>
2022-12-07 14:26:41 -0800

    updates models with fields

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
-# class AutomobileVO(models.Model):
-#     vin = models.CharField(max_length=17, unique=True)
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+
+    def __str__(self):
+        return self.vin
 
 class Technician(models.Model):
     name = models.CharField(max_length=100)
@@ -12,9 +15,10 @@ class Technician(models.Model):
 
 class Appointment(models.Model):
     customer_name = models.CharField(max_length=150)
-    appointment_time = models.DateTimeField()
+    time = models.DateTimeField()
     reason = models.CharField(max_length=200)
-    customer_vin = models.CharField(max_length=17)
+    vehicle_vin = models.CharField(max_length=17)
+    discount = models.BooleanField(default=False)
 
     technician = models.ForeignKey(
         Technician,
@@ -22,9 +26,8 @@ class Appointment(models.Model):
         on_delete=models.CASCADE,
     )
 
-    # automobile = models.ForeignKey(
-    #     AutomobileVO,
-    #     related_name="appointments",
-    #     on_delete=models.CASCADE,
-    #     null=True
-    # )
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="appointments",
+        on_delete=models.CASCADE,
+    )
