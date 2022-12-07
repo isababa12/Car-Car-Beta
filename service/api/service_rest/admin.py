170dc4b269e2b38beb751079050db8e6ffdb164e
Ryan <ryanlu294@gmail.com>
2022-12-07 11:13:26 -0800

    commit to push inventory front end to main

--- service/api/service_rest/admin.py
+++ service/api/service_rest/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AutomobileVO, Appointment, Technician
 
-# Register your models here.
+admin.site.register(AutomobileVO)
+admin.site.register(Technician)
+admin.site.register(Appointment)
--- service/poll/poller.py
+++ service/poll/poller.py
@@ -17,14 +17,13 @@ def poll():
     while True:
         print('Service poller polling for data')
         try:
-            response = requests.get("http://inventory-api:8000/api/automobiles/")
-            content = json.loads(response.content)
-            for auto in content["autos"]:
-                AutomobileVO.objects.update_or_create(
-                    defaults={
-                        "vin":auto["vin"],
-                    }
-                )
+            # response = requests.get("http://inventory-api:8000/api/automobiles/")
+            # content = json.loads(response.content)
+            # for auto in content["autos"]:
+            #     AutomobileVO.objects.update_or_create(
+            #         {"vin":auto["vin"]}
+            #     )
+            pass
         except Exception as e:
             print(e, file=sys.stderr)
         time.sleep(60)
