197e7c8925226039492a6c0c6c63461cb0e1e168
Ryan <ryanlu294@gmail.com>
2022-12-06 17:47:16 -0800

    adds models and views

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from .models import Technician, Appointment
+from common.json import ModelEncoder
 
-# Create your views here.
+
+class TechnicianEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        "name",
+        "employee_number",
+    ]
+
+@require_http_methods(["GET", "POST"])
+def api_technicians(request):
+    if request.method == "GET":
+        technicians = Technician.objects.all()
+        return JsonResponse(
+            {"technicians": technicians},
+            encoder=TechnicianEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        technician = Technician.objects.create(**content)
+        return JsonResponse(
+            technician,
+            encoder=TechnicianEncoder,
+            safe=False,
+        )
