cc750b71a470453c863a2f36b1dd26e13ea7e818
Ryan <ryanlu294@gmail.com>
2022-12-08 12:38:04 -0800

    adds complete and cancel buttons to appointment frontend

--- ghi/app/src/AppointmentList.js
+++ ghi/app/src/AppointmentList.js
@@ -25,6 +25,34 @@ class AppointmentList extends React.Component {
     }
   }
 
+  async cancelAppointment(id) {
+    const url = `http://localhost:8080/api/appointments/${id}/cancel/`;
+    const fetchConfig = {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(url, fetchConfig);
+    if (response.ok) {
+      this.componentDidMount();
+    }
+  }
+
+  async completeAppointment(id) {
+    const url = `http://localhost:8080/api/appointments/${id}/complete/`;
+    const fetchConfig = {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(url, fetchConfig);
+    if (response.ok) {
+      this.componentDidMount();
+    }
+  }
+
   render() {
     return (
       <>
@@ -45,17 +73,42 @@ class AppointmentList extends React.Component {
             {this.state.appointments.map((appointment) => {
               const dateObj = new Date(appointment.time);
               const options = { timeStyle: "short" };
-              return (
-                <tr key={appointment.id}>
-                  <td>{appointment.vehicle_vin}</td>
-                  <td>{appointment.customer_name}</td>
-                  <td>{dateObj.toLocaleDateString()}</td>
-                  <td>{dateObj.toLocaleTimeString([], options)}</td>
-                  <td>{appointment.technician.name}</td>
-                  <td>{appointment.reason}</td>
-                  {this.discountCheck(appointment.discount)}
-                </tr>
-              );
+              if (
+                appointment.canceled === false &&
+                appointment.completed === false
+              ) {
+                return (
+                  <tr key={appointment.id}>
+                    <td>{appointment.vehicle_vin}</td>
+                    <td>{appointment.customer_name}</td>
+                    <td>{dateObj.toLocaleDateString()}</td>
+                    <td>{dateObj.toLocaleTimeString([], options)}</td>
+                    <td>{appointment.technician.name}</td>
+                    <td>{appointment.reason}</td>
+                    {this.discountCheck(appointment.discount)}
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-danger"
+                        onClick={(e) =>
+                          this.cancelAppointment(appointment.id, e)
+                        }
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-success"
+                        onClick={(e) =>
+                          this.completeAppointment(appointment.id, e)
+                        }
+                      >
+                        Completed
+                      </button>
+                    </td>
+                  </tr>
+                );
+              }
             })}
           </tbody>
         </table>
